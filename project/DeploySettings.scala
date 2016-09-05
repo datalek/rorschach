7f36c159a51468e7f565e744da65d772858ee0da
datalek <ferlosalem@gmail.com>
2016-09-05 19:44:14 +0200

    removes bintrayReleaseOnPublish on snapshot build

--- project/DeploySettings.scala
+++ project/DeploySettings.scala
@@ -19,6 +19,7 @@ object PublishSettings {
 
   private val snapshot = Seq(
     publishTo := {if(isSnapshot.value) Some("snapshots" at "http://oss.jfrog.org/artifactory/oss-snapshot-local") else publishTo.value},
+    bintray.BintrayKeys.bintrayReleaseOnPublish := !isSnapshot.value,
     // Only setting the credentials file if it exists (#52)
     credentials := {if (isSnapshot.value) List(Path.userHome / ".bintray" / ".artifactory").filter(_.exists).map(Credentials(_)) else credentials.value}
   )
@@ -27,7 +28,7 @@ object PublishSettings {
     pomExtra := pom,
     publishArtifact in Test := false,
     homepage := Some(url("https://github.com/datalek/rorschach")),
-    resolvers += Resolver.url("supler ivy resolver", url("http://dl.bintray.com/merle/maven"))(Resolver.ivyStylePatterns),
+    resolvers += Resolver.url("ivy resolver", url("http://dl.bintray.com/merle/maven"))(Resolver.ivyStylePatterns),
     licenses := ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil // this is required! otherwise Bintray will reject the code
   )
 }
