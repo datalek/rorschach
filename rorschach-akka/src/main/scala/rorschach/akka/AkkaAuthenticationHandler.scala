bc4c791657ebf01f7e76a5275604029e5dcdefbb
datalek <ferlosalem@gmail.com>
2017-10-11 21:08:41 +0200

    Change AuthenticationHandler move type parameter A <: Authenticator as type member

--- rorschach-akka/src/main/scala/rorschach/akka/AkkaAuthenticationHandler.scala
+++ rorschach-akka/src/main/scala/rorschach/akka/AkkaAuthenticationHandler.scala
@@ -7,9 +7,24 @@ import rorschach.core.extractors._
 import rorschach.core.services._
 import scala.concurrent.ExecutionContext
 
-case class AkkaAuthenticationHandler[A <: Authenticator, I](
-  authenticatorExtractor: AuthenticatorExtractor[RequestContext, A],
-  authenticatorService: AuthenticatorService[A],
-  identityExtractor: IdentityRetriever[LoginInfo, I],
-  authenticatorEmbedder: AuthenticatorEmbedder[A, Directive0]
-)(implicit val ec: ExecutionContext) extends AuthenticationHandler[RequestContext, A, I, Directive0]
+trait AkkaAuthenticationHandler[I]
+  extends AuthenticationHandler[RequestContext, I, Directive0]
+
+object AkkaAuthenticationHandler {
+  def apply[A <: Authenticator, I](
+    authenticatorExtractor: AuthenticatorExtractor[RequestContext, A],
+    authenticatorService: AuthenticatorService[A],
+    identityExtractor: IdentityRetriever[LoginInfo, I],
+    authenticatorEmbedder: AuthenticatorEmbedder[A, Directive0]
+  )(implicit ec: ExecutionContext): AkkaAuthenticationHandler[I] =
+    AkkaAuthenticationHandlerImpl(authenticatorExtractor, authenticatorService, identityExtractor, authenticatorEmbedder)(ec)
+
+  case class AkkaAuthenticationHandlerImpl[A0 <: Authenticator, I](
+      authenticatorExtractor: AuthenticatorExtractor[RequestContext, A0],
+      authenticatorService: AuthenticatorService[A0],
+      identityExtractor: IdentityRetriever[LoginInfo, I],
+      authenticatorEmbedder: AuthenticatorEmbedder[A0, Directive0]
+  )(implicit val ec: ExecutionContext) extends AkkaAuthenticationHandler[I] {
+    type A = A0
+  }
+}
--- rorschach-akka/src/main/scala/rorschach/akka/directives/SecurityDirectives.scala
+++ rorschach-akka/src/main/scala/rorschach/akka/directives/SecurityDirectives.scala
@@ -13,7 +13,7 @@ trait SecurityDirectives {
   /**
    * Provide an authenticator from a loginInfo
    */
-  def create[A <: Authenticator, T](magnet: RorschachAuthMagnet[A, T], loginInfo: LoginInfo): Directive1[A] = ??? //magnet.create(loginInfo)
+  def create[A <: Authenticator, T](magnet: RorschachAuthMagnet[T], loginInfo: LoginInfo): Directive1[A] = ??? //magnet.create(loginInfo)
 
   /**
    * Given an authenticator embed the serialized value and provide this value on inner route
@@ -26,23 +26,23 @@ trait SecurityDirectives {
    * Discard authenticator if there is one, otherwise do nothing.
    * Examples: if authenticator is a token it will be remove, if is a cookie it wil be discard, etc...
    */
-  def discard[A <: Authenticator, T](magnet: RorschachAuthMagnet[A, T]): Directive0 = ??? //magnet.discard
+  def discard[A <: Authenticator, T](magnet: RorschachAuthMagnet[T]): Directive0 = ??? //magnet.discard
 
   /**
    * Wraps its inner Route with authentication support.
    * Can be called either with a ``Future[Authentication[T]]`` or ``ContextAuthenticator[T]``.
    */
-  def authenticate[A <: Authenticator, T](magnet: RorschachAuthMagnet[A, T]): Directive1[T] = magnet.authenticate
+  def authenticate[A <: Authenticator, T](magnet: RorschachAuthMagnet[T]): Directive1[T] = magnet.authenticate
 
   /**
    * Wraps its inner Route with optional authentication support.
    * Can be called either with a ``Future[Authentication[T]]`` or ``ContextAuthenticator[T]``.
    */
-  def optionalAuthenticate[A <: Authenticator, T](magnet: RorschachAuthMagnet[A, T]): Directive1[Option[T]] = magnet.optionalAuthenticate
+  def optionalAuthenticate[A <: Authenticator, T](magnet: RorschachAuthMagnet[T]): Directive1[Option[T]] = magnet.optionalAuthenticate
 
 }
 
-class RorschachAuthMagnet[A <: Authenticator, T](authenticator: AkkaAuthenticationHandler[A, T])(implicit executor: ExecutionContext) {
+class RorschachAuthMagnet[T](authenticator: AkkaAuthenticationHandler[T])(implicit executor: ExecutionContext) {
 
   val authenticate: Directive1[T] = extract(authenticator.authenticate).flatMap(onSuccess(_)).flatMap {
     case Right((auth, user, d0)) ⇒ provide(user) & d0
@@ -65,7 +65,7 @@ class RorschachAuthMagnet[A <: Authenticator, T](authenticator: AkkaAuthenticati
 }
 
 object RorschachAuthMagnet {
-  implicit def fromRorschachAuthentication[A <: Authenticator, T](authenticator: AkkaAuthenticationHandler[A, T])(implicit executor: ExecutionContext): RorschachAuthMagnet[A, T] = {
-    new RorschachAuthMagnet[A, T](authenticator)
+  implicit def fromRorschachAuthentication[T](authenticator: AkkaAuthenticationHandler[T])(implicit executor: ExecutionContext): RorschachAuthMagnet[T] = {
+    new RorschachAuthMagnet[T](authenticator)
   }
 }
